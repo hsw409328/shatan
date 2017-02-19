f5cb26c233833e585b170ba585569093cf4bf0a9
hsw409328 <409328820@qq.com>
2017-02-19 15:29:53 +0800

    js

---
+++ public/js/common.js
@@ -0,0 +1,43 @@
+/**
+ * Created by MrHao on 2017/2/14.
+ */
+var AjaxCommon = {
+    url: '/c/',
+    data: '',
+    method: '',
+    callback_func: null,
+    post: function () {
+        $.post(AjaxCommon.url, AjaxCommon.data, AjaxCommon.callback);
+    },
+    get: function () {
+        $.get(AjaxCommon.url, AjaxCommon.callback);
+    },
+    callback: function (data, status) {
+        AjaxCommon.callback_func(data, status);
+    }
+};
+
+/**
+ AjaxCommon.data = {"action":"b","run":"c"};
+ AjaxCommon.callback_func = function(data,status){
+      console.log(data);
+    };
+ AjaxCommon.post();
+ */
+
+var UtilCommon = {
+    website: 'http://lvyou.com',
+    href: function (url) {
+        window.location.href = UtilCommon.website + url;
+    },
+    reload: function () {
+        window.location.reload();
+    },
+    back: function () {
+        window.history.back();
+    },
+    parseJson: function (data) {
+        return eval('(' + data + ')');
+    },
+};
+
